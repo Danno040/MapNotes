8d21a12a7fde862b66e728a7d32031da728e0de8
Mike F <mfeineman@room214.com>
2012-06-28 20:05:32 -0600

    Initial Commit

---
+++ MapNotes/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MapNotes
+//
+//  Created by Mike Feineman on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class MainViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) MainViewController *mainViewController;
+
+@end
---
+++ MapNotes/MainViewController.h
@@ -0,0 +1,15 @@
+//
+//  MainViewController.h
+//  MapNotes
+//
+//  Created by Mike Feineman on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlipsideViewController.h"
+
+@interface MainViewController : UIViewController <FlipsideViewControllerDelegate>
+
+- (IBAction)showInfo:(id)sender;
+
+@end
